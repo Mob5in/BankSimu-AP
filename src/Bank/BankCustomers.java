1b431bd7129af59efe3740d13f2f1682396486ce
Mobin <Mobinmojiri1234@gmail.com>
2025-03-17 17:11:45 +0330

    make customer class method

--- src/Bank/BankCustomers.java
+++ src/Bank/BankCustomers.java
@@ -1,4 +1,30 @@
 package Bank;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class BankCustomers {
+
+    private ArrayList<BankAccount> accountsList = new ArrayList<>();
+
+    public void addAccount(BankAccount account){
+        accountsList.add(account);
+    }
+
+    public void showAllBalances(){
+        System.out.println("Account Number              balance");
+        for(BankAccount account: accountsList){
+            System.out.println(account.getAccountNumber()+"             "+account.getBalance());
+        }
+
+    }
+
+    public BankAccount findAccount(String accountNumber){
+        for(BankAccount account: accountsList){
+            if(Objects.equals(accountNumber, account.getAccountNumber())){
+                return account;
+            }
+        }
+        System.out.println("Wrong account number");
+        return null;
+    }
 }
